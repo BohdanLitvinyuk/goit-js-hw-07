82758b2a2ee6796723db7ba09fe10dc16ac6b366
Bohdan Lytvyniuk <lytvyniukbohdan@gmail.com>
2023-05-27 23:20:53 +0200

    add img gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const cardsContainer = createGalleryCards(galleryItems);
+const galleryListEl = document.querySelector(".gallery");
+
+galleryListEl.insertAdjacentHTML('beforeend', cardsContainer);
+galleryListEl.addEventListener('click', onGalleryListItemClick);
+
+function createGalleryCards(galleryItems) {
+   return galleryItems.map(({ original, preview, description }) => {
+        return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    }).join("");
+}
+function onGalleryListItemClick(event) {
+    event.preventDefault();
+    console.log(event)
+}
